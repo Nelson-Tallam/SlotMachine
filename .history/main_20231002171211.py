7ec9a174924c82bab4b5d83c02c938b783cfef5c
nelson-tallam <you@example.com>
2023-10-03 14:33:23 +0300

    Complete slot machine

---
+++ .history/main_20231002171211.py
@@ -0,0 +1,56 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+def deposit():
+    while True:
+        amount = input("What amount would you like to deposit?Ksh.")
+        if amount.isdigit():
+            amount= int(amount)
+            if amount>0:
+                break
+            else:
+                print("Amount must be more than 0.")
+        else:
+            print("Please enter a number")
+    return amount
+
+
+def get_number_of_lines():
+
+    while True:
+        lines = input("Enter the number of lines to bet on(1-"+ str(MAX_LINES)+")?")
+        if lines.isdigit():
+            lines= int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number")
+    return lines
+
+def get_bet():
+    while True:
+            amount = input("What amount would you like to bet on each line?Ksh.")
+            if amount.isdigit():
+                    amount= int(amount)
+                    if MIN_BET<= amount<= MAX_BET:
+                        break
+                    else:
+                        print(f"Amount must be between Ksh{MIN_BET}- Ksh{MAX_BET}.")
+            else:
+                    print("Please enter a number")
+    return amount
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        
+    bet = get_bet() 
+    total_bet = bet * lines
+    print (f"You are betting Ksh. {bet} on {lines} lines. Your total bet amount is: Ksh.{total_bet}")
+
+main()
+
